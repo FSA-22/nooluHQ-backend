79b142487c14e0407301ee560d22480deeaab644
FSA-22 <simeonstat@gmail.com>
2026-03-31 14:11:58 +0000

    fix: fix production nodemailer failure

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,25 +1,19 @@
 import nodemailer from 'nodemailer';
 import { EMAIL_PASSWORD, EMAIL_USER, EMAIL_HOST, EMAIL_PORT } from '../config/env.js';
-import SMTPTransport from 'nodemailer/lib/smtp-transport/index.js';
 
-// Create transporter (handles sending)
 const transporter = nodemailer.createTransport({
   host: EMAIL_HOST || 'smtp.gmail.com',
-  port: Number(EMAIL_PORT) || 465,
-  secure: true,
+  port: Number(EMAIL_PORT) || 587,
+  secure: false,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASSWORD,
   },
-  tls: {
-    family: 4, // force IPv4
-  },
-} as SMTPTransport.Options);
+});
 
-// Create function to send emails
 const sendEmail = async (to: string, subject: string, text: string) => {
   await transporter.sendMail({
-    from: `"Up-cut" <${EMAIL_USER}>`, // sender name and email
+    from: `"Up-cut" <${EMAIL_USER}>`,
     to,
     subject,
     text,
